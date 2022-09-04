f8fc82ebdccd82e6d318e9f095a9f31129a3aa39
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-09-04 11:24:01 -0400

    prettier format

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,9 +1,11 @@
-import contentful from 'contentful'
+import contentful from "contentful";
 
 const client = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-	host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
 
-export default client
+export default client;
